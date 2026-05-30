149bc8f07241ed1875a6b29594d1b9a2676f7750
mau <stevenetworkesp267@gmail.com>
2026-05-30 11:21:47 -0300

    Create setup.py

---
+++ library/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="forge-decoder",
+  version="0.1.1",
+  author="MDFJ"
+  python_requires=">=3.10",
+install_requires=[
+"torch",
+  "forge",
+  "ai",
+],
+)
